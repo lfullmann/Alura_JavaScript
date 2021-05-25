280c6c7b1b7bc14fc35dc3bf79c8a1cbc0bdaafa
lfullmann <filippemkt@gmail.com>
2021-05-25 11:20:56 -0300

    Buscando dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,20 @@
-console.log("Arquivo externo");
-
 var titulo = document.querySelector("h1");
 titulo.textContent = "Teste título";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+// console.log(paciente);
+// // console.log(tdPeso);
+// // console.log(peso);
+// console.log(tdAltura);
+// console.log(altura);
+
+
