37604119584914ce5d5e3e0bcda5a7ca3c0f8059
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-05-31 18:23:02 -0300

    Validacao de calculo (Looping for)

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,39 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Teste título";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  var tdImc = paciente.querySelector(".info-imc");
 
-//Validacao peso
-if (peso <= 0 || peso >= 1000) {
-  console.log("Peso inválido");
-  pesoEhValido = false;
-  tdImc.textContent = "Peso inválido";
-}
+  var pesoEhValido = true;
+  var alturaEhValida = true;
 
-//Validacao altura
-if (altura <= 0 || altura >= 3.0) {
-  console.log("Altura inválida");
-  alturaEhValida = false;
-  tdImc.textContent = "Altura inválida";
-}
+  //Validacao peso
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+  }
 
-if (pesoEhValido && alturaEhValida) {
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
+  //Validacao altura
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+  }
+
+  if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
 
 // console.log(imc);
