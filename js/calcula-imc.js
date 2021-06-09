4d7d4fb1abc9fdadb7d1fde4af1292e9bf2d9d79
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-06-09 16:48:25 -0300

    Adicionando funções

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,30 +1,45 @@
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function (event) {
-  event.preventDefault();
+var titulo = document.querySelector("h1");
+titulo.textContent = "Teste título";
 
-  var form = document.querySelector("#form-adiciona");
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-  var pacienteTr = document.createElement("tr");
-  var nomeTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td");
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
+  var tdImc = paciente.querySelector(".info-imc");
 
-  var tabela = document.querySelector("#tabela-pacientes");
-  tabela.appendChild(pacienteTr);
-});
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  //Validacao peso
+  if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  //Validacao altura
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida";
+    paciente.classList.add("paciente-invalido");
+  }
+  //Calculo de IMC
+  if (pesoEhValido && alturaEhValida) {
+    var imc = calculaImc(peso, altura);
+    tdImc.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
