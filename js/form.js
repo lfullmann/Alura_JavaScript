0cea55b3f5efb5d2fb639647ca92d359517cb99f
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-06-09 21:43:38 -0300

    Validando os demais campos

--- js/form.js
+++ js/form.js
@@ -11,8 +11,7 @@ botaoAdicionar.addEventListener("click", function (event) {
   var erros = validaPaciente(paciente);
 
   if (erros.length > 0) {
-    var mensagemDeErro = document.querySelector("#mensagem-erro");
-    mensagemDeErro.textContent = erros;
+    exibeMensagensDeErro(erros);
     return;
   }
 
@@ -21,7 +20,20 @@ botaoAdicionar.addEventListener("click", function (event) {
   tabela.appendChild(pacienteTr);
 
   form.reset();
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros) {
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function (erro) {
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
 
 function obtemPacienteDoFormulario(form) {
   var paciente = {
@@ -59,11 +71,23 @@ function montaTd(dado, classe) {
 function validaPaciente(paciente) {
   var erros = [];
 
+  if (paciente.nome.length == 0) {
+    erros.push("O nome não pode ser em branco");
+  }
   if (!validaPeso(paciente.peso)) {
     erros.push("Peso é inválido");
   }
   if (!validaAltura(paciente.altura)) {
-    erros.push("Altua é inválida!");
+    erros.push("Altura é inválida!");
+  }
+  if (paciente.gordura.length == 0) {
+    erros.push("A gorduda não pode ser em branco");
+  }
+  if (paciente.peso.length == 0) {
+    erros.push("O peso não pode ser em branco");
+  }
+  if (paciente.altura.length == 0) {
+    erros.push("A altura não pode ser em branco");
   }
   return erros;
 }
